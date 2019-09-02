4f35384eaddb1d6a492cf6cd9f16403e2bf52c79
Woohyeok Aaron Kim <woohyeok.kim@rakuten.com>
2019-09-02 19:25:03 +0900

    UnaryOperator

--- Lambda/src/app/App.java
+++ Lambda/src/app/App.java
@@ -1,6 +1,9 @@
 package app;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -17,5 +20,15 @@ public class App {
         // 아 이렇게 정의를 해두고 쓰는구나.
         BiFunction<Double, Integer, Double> val = (t1, t2) -> t1 + t2;
         System.out.println(val.apply(10.0, 10));
+
+        System.out.println("************************");
+
+        List<Integer> codes = Arrays.asList(10, 20);
+        UnaryOperator<Double> uo = s -> s + 10.0;
+        // codes.replaceAll(uo); compile error... Integer != Double
+
+        System.out.println("************************");
+
+        
     }
 }
